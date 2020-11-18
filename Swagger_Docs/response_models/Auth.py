bf850834d929d5b8177b58a7362e8ad6c95ae497
JaoJaoLin <jame31560@gmail.com>
2020-11-18 20:58:16 +0800

    Fix requirements.txt & remove duplic import

--- Swagger_Docs/response_models/Auth.py
+++ Swagger_Docs/response_models/Auth.py
@@ -1,5 +1,4 @@
 from flask_restful_swagger_2 import Schema
-from flask_restful_swagger_2 import Schema
 
 
 class Auth_Create_Res(Schema):
@@ -8,8 +7,8 @@ class Auth_Create_Res(Schema):
         "email": {"type": "string"},
         "id": {"type": "string"},
         "name": {"type": "string"},
-        "point": {"type": "int"},
-        "role": {"type": "int"},
+        "point": {"type": "integer"},
+        "role": {"type": "integer"},
         "token": {"type": "string"},
         "username": {"type": "string"},
     }
