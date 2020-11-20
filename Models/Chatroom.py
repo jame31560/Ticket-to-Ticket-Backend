d886818ba9ccc525f015d828b86860b8ec6239d3
zihao <whatsup0325@gmail.com>
2020-11-20 19:34:38 +0800

    [Update]chat room

--- Models/Chatroom.py
+++ Models/Chatroom.py
@@ -2,10 +2,14 @@ import mongoengine as db
 from .Message import Message
 
 class Chatroom(db.Document):
+    roomname = db.StringField()
     Messagelist = db.EmbeddedDocumentListField(Message)
 
-    def createChatroom(self):
+    def createChatroom(self,name:str):
+        self.roomname = name
         self.Messagelist = []
+        self.save()
         
     def appendMessage(self,msg:Message):
         self.Messagelist.append(msg)
+        self.save()
